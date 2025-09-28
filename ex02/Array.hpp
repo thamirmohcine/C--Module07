88fdf9be492eb6c9fa1cf1a6383e138ef8bde7bb
thamirmohcine <thamirmohsine@gmail.com>
2025-09-28 01:12:07 +0100

    FINISH 07

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,18 +1,42 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
+#include <iostream>
+#include <exception>
 
 template<typename T>
-class Array : std::exception {
+class Array{
         T *Arr;
         size_t ArrSize;
     public:
-        Array():Arr(new T[]), ArrSize(0){};
-        Array(unsigned int){int *a =  new int(); std::cout << *a < std::endl;}
-        Array& operator=(const Array& other){}
-        T& operator[](size_t index);
+        Array():Arr(NULL), ArrSize(0){}
+        Array(unsigned int n):Arr(new T[n]()), ArrSize(n){}
+        Array(const Array& other):Arr(NULL), ArrSize(0){ *this = other;}
+        Array& operator=(const Array& other){
+            if (this != &other){
+                delete[] Arr;
+                if (other.ArrSize > 0){
+                    this->Arr = new T[other.ArrSize];
+                    for (int i(0); i < other.ArrSize;i++){ this->Arr[i] = other.Arr[i];}
+                    this->ArrSize = other.ArrSize;
+                }
+                else
+                    Arr = NULL;
+            }
+            return *this;
+        }
+        ~Array(){ if (ArrSize > 0) {delete[] Arr;}}
+        T& operator[](size_t index){
+            if (index >= ArrSize)
+                throw std::exception();
+            return Arr[index];
+        }
+        const T& operator[](size_t index) const {
+            if (index >= ArrSize)
+                throw std::exception();
+            return Arr[index];
+        }
         size_t size() const{return ArrSize;};
 };
-
 
 
 #endif 
